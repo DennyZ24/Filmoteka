8fa485b612f78d8581320a7fd1acf58465c7ae05
Denis Zapara <dzapara24@gmail.com>
2021-08-09 11:39:16 +0300

    add new file and some style css

---
+++ src/js/refs.js
@@ -0,0 +1,5 @@
+export default {
+  input: document.querySelector('.js-input'),
+  homeBtn: document.querySelector('button[data-name="home"]'),
+  libBtn: document.querySelector('button[data-name="library"]')
+};
